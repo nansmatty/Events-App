0f2b8c7ffa5496e5694b5812aec8821b4b5cbba3
narayanJewellerskart <narayan@jewellerskart.com>
2023-03-01 09:46:22 +0530

    some changes

--- components/event-detail/event-summary.js
+++ components/event-detail/event-summary.js
@@ -1,13 +1,15 @@
-import classes from './event-summary.module.css';
+import classes from "./event-summary.module.css";
 
 function EventSummary(props) {
-  const { title } = props;
+	const { title } = props;
 
-  return (
-    <section className={classes.summary}>
-      <h1>{title}</h1>
-    </section>
-  );
+	return (
+		<div>
+			<section className={classes.summary}>
+				<h1>{title}</h1>
+			</section>
+		</div>
+	);
 }
 
 export default EventSummary;
