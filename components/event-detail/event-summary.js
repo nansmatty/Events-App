8b57c897069bec4b99a8427e7d4ff36f9f00e86d
narayanJewellerskart <narayan@jewellerskart.com>
2023-03-01 09:47:22 +0530

    revert back some changes

--- components/event-detail/event-summary.js
+++ components/event-detail/event-summary.js
@@ -4,11 +4,9 @@ function EventSummary(props) {
 	const { title } = props;
 
 	return (
-		<div>
-			<section className={classes.summary}>
-				<h1>{title}</h1>
-			</section>
-		</div>
+		<section className={classes.summary}>
+			<h1>{title}</h1>
+		</section>
 	);
 }
 
